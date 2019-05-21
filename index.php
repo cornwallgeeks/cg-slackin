9306b7b875e455497958fcb467349577a7db6b9a
Dan Reeves <hey@danreev.es>
2019-05-21 16:10:22 +0100

    use a forever link

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-  header("Location: https://join.slack.com/t/cornwall-geeks/shared_invite/enQtNjAzMDQyODAwOTAyLWM0ZGNjMTNkMDA2MjkxMDlkOTM5NGVlZTk3MTk1NjdmOWVjNGU1YzFjZmU4MjIwMzQyZjEwMmEyYjRkZTU1NjQ");
+  header("Location: https://join.slack.com/t/cornwall-geeks/shared_invite/enQtNjMwNDI3Mzc3OTA2LTdiYzUwNTBlZjY2MDllNGE0YzkxYzlkZDMxOWRiYjhiZjU2NDhlN2FhNzUxNTNiYWFhMjRjMzUyYTlmNmYxZDM");
