7e4d9b0a29443fd33946fc0741956884c69c0370
Dan Reeves <hey@danreev.es>
2019-09-26 13:05:31 +0100

    real infinite link

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-  header("Location: https://join.slack.com/t/cornwall-geeks/shared_invite/enQtNjMwNDI3Mzc3OTA2LTdiYzUwNTBlZjY2MDllNGE0YzkxYzlkZDMxOWRiYjhiZjU2NDhlN2FhNzUxNTNiYWFhMjRjMzUyYTlmNmYxZDM");
+  header("Location: https://join.slack.com/t/cornwall-geeks/shared_invite/enQtNjMwNDI3Mzc3OTA2LWQwNDRlZWZmOTRmYzZkOTQxNDIyYzNiYjQxYWZkNDliMTliYTRkOWFkMTg0ZTY4MmM5NjUwN2E1MGJhYWJkMjg");
