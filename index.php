0163641da3792616add64057d6c3c10ca2209123
Dan Reeves <hey@danreev.es>
2020-02-28 10:32:01 -0800

    fix, its simple now

--- index.php
+++
@@ -1,2 +0,0 @@
-<?php
-  header("Location: https://join.slack.com/t/cornwall-geeks/shared_invite/enQtNjMwNDI3Mzc3OTA2LWQwNDRlZWZmOTRmYzZkOTQxNDIyYzNiYjQxYWZkNDliMTliYTRkOWFkMTg0ZTY4MmM5NjUwN2E1MGJhYWJkMjg");
